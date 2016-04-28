55fd08e9d302403d36415cc3ee5ca375f5e23dcb
Amellado <agustin.mellado@alumnos.usm.cl>
2016-04-28 13:17:03 -0300

    seteando vistas
    recuerden definir bien el root directory de apache o las rutas no
    funcionan

---
+++ app/Http/Controllers/EstableExampleController.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class EstableExampleController extends Controller
+{
+    public function index(){
+    	return view('login.index');
+ 	   }
+}
---
+++ resources/views/app.blade.php
@@ -0,0 +1,14 @@
+<!doctype html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>BrowserPrinting</title>
+	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/boostrap/3.2.0/css/bootstrap.min.css">
+
+</head>
+<body>
+	<div class="container">
+		@yield('content')
+	</div>
+</body>
+</html>
