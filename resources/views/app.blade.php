d5e24e4f3058ae2214cef8b142be6cd03efce6a4
Amellado <agustin.mellado@alumnos.usm.cl>
2016-04-28 22:03:10 -0300

    navbar en app
    paso al ps

--- resources/views/app.blade.php
+++ resources/views/app.blade.php
@@ -3,12 +3,32 @@
 <head>
 	<meta charset="UTF-8">
 	<title>BrowserPrinting</title>
-	<link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/boostrap/3.2.0/css/bootstrap.min.css">
-
+	<link href="https://maxcdn.bootstrapcdn.com/bootswatch/3.3.6/sandstone/bootstrap.min.css" rel="stylesheet" integrity="sha384-QqZs0aSOGFan3GWVdP4PyCBp6icaF/4n8Q+wsmZUTiiVIqE4r4tFYcb8Osf+8y45" crossorigin="anonymous">
+	<style type="text/css"> body { padding-top: 70px; } .mygrid-wrapper-div {overflow: scroll; height: 300px; } </style>
 </head>
 <body>
+	<nav class="navbar navbar-default navbar-fixed-top" role="navigation">
+    <div class="container">
+      <div class="navbar-header">
+        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+          <span class="sr-only">Toggle navigation</span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+          <span class="icon-bar"></span>
+        </button>
+        <a class="navbar-brand" href="">BrowserPrint Lab</a>
+      </div>
+      <div id="navbar" class="collapse navbar-collapse">
+        <ul class="nav navbar-nav">
+          <li><a href="/login">Clean Canvas Fingerprint</a></li>
+        </ul>
+      </div><!--/.nav-collapse -->
+    </div>
+  </nav>
 	<div class="container">
 		@yield('content')
 	</div>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+	<script src="https://code.jquery.com/jquery-1.12.3.min.js" integrity="sha256-aaODHAgvwQW1bFOGXMeX+pC4PZIPsvn2h1sArYOhgXQ=" crossorigin="anonymous"></script>
 </body>
 </html>
