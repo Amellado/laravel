84966fb8b9675d2967d6f275269479b9a5f3b19b
Amellado <agustin.mellado@alumnos.usm.cl>
2016-05-19 15:45:15 -0300

    cambio de texto visible
    Falta hacer que todo espere a que el AJAX inicial del ejemplo 2 termine
    antes de dejar hacer la transición o todo falla.

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,5 @@
     <p class="lead">This page contains two example uses of fingerprintToken.js, which is the result of the thesis "SISTEMAS DE AUTENTIFICACIÓN EN SISTEMAS WEB BASADOS EN BROWSER FINGER PRINTING "</p>
     <p>The first example is about the user login function, it's designed to upgrade a username-password credential to a username-password-fingerprint one. The fingerprint used here is a canvas based one.</p>
 
-    <p>The second example covers the use of the short term authentication between views, used to prevent session_hijack attacks and such.</p>
-
-
+    <p>The second example covers the use of the short term authentication between views, used as an alternative to cookies or similar things.</p>
 @stop
